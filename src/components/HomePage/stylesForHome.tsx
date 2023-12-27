ec9e6c87a2e348dcf97f2c02e21c5046c51d2d61
Dinora <acullen647@gmail.com>
2023-12-27 13:41:32 +0500

    changed some mistakes

---
+++ src/components/HomePage/stylesForHome.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const stylesForHome = () => {
+  return <div>stylesForHome</div>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import { MainHomePage } from "./Pages/MainHomePage";
 import SignIn from "./Pages/SignIn";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-ReactDOM.render(
+// import reportWebVitals from "./reportWebVitals";
+
+render(
   <React.StrictMode>
     <Router>
       <Routes>
