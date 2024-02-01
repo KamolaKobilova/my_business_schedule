aa63f7438db1b4f43a1db7d68357a04604328144
Kamola Kobilova <kamola_8880@mail.ru>
2024-02-01 16:27:53 +0500

    dropdown

--- src/Pages/MainHomePage/BookingModal/HomePageNav.tsx
+++ src/Pages/MainHomePage/BookingModal/HomePageNav.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { NavButton, Navbar, NavRight } from "./BookingStyles";
 import { useState } from "react";
+import { Menu, Dropdown } from 'antd';
 import { CalendarModal } from "./BookingPage";
-import {PlusOutlined, BellOutlined, SettingOutlined} from "@ant-design/icons";
-import logo from '../../../assets/BookingPage/booking.png'
+import {PlusOutlined, BellOutlined, SettingOutlined,CheckSquareOutlined,FieldTimeOutlined,AppstoreOutlined} from "@ant-design/icons";
+import logo from '../../../assets/BookingPage/booking.png';
+
 
 
 export const HomePageNav = () => {
+
+  const menu = (
+    <Menu style={{width:"260px", height:"400px"}}>
+         <Menu.Item key="1"><CheckSquareOutlined style={{marginRight:"5px"}}/>Appointment</Menu.Item>
+         <Menu.Item key="2"><FieldTimeOutlined style={{marginRight:"5px"}}/>Add break & Block time</Menu.Item>
+         <Menu.Item key="3"><BellOutlined style={{marginRight:"5px"}}/>Add special working hours</Menu.Item>
+         <div style={{width:"200px", marginLeft:"28px"}}> <hr /></div>
+         <Menu.Item key="5"><AppstoreOutlined/>Workspaces</Menu.Item>
+         <Menu.Item key="6">Staff</Menu.Item>
+         <Menu.Item key="7">Service</Menu.Item>
+         <Menu.Item key="8">Customer</Menu.Item>
+         <Menu.Item key="4">Resources</Menu.Item>
+    </Menu>
+  );
+        
+
+  
+
+
   const [isHovered, setIsHovered] = useState(false);
   const [isCHanged, setIsChanged] = useState(false);
   const [isCHanging, setIsChanging] = useState(false);
@@ -22,10 +43,15 @@ export const HomePageNav = () => {
     setIsModalOpen(false);
     console.log("modal closed");
   };
+    
+
+
+
+
   return (
     <Navbar>
       <div></div>
-      <NavButton><img src={logo} alt="" style={{width: "30px", backgroundColor: "red" ,marginTop: "12px"}}/>  <span>Booking</span></NavButton>
+      <NavButton style={{display:"flex", alignItems:"center"}}><img src={logo} alt="" style={{width: "30px"}}/> Booking</NavButton>
       <NavButton style={{ color: isCHanging ? '#F8F8F8' : '#D3D3D3'}} 
       onMouseEnter={() => setIsChanging(true)}
       onMouseLeave={() => setIsChanging(false)}
@@ -46,21 +72,24 @@ export const HomePageNav = () => {
           />
           </NavRight>  
       
-        <NavRight><PlusOutlined
-         style={{ fontSize: '24px',
-         color: isHovered ? '#F8F8F8' : '#D3D3D3',
-        }}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-          /></NavRight>  
-        <NavRight><BellOutlined
+        <NavRight> <Dropdown overlay={menu} placement="bottomRight" trigger={['click']}>
+          <PlusOutlined
+            style={{
+              fontSize: '24px',
+              color: isHovered ? '#F8F8F8' : '#D3D3D3',
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          />
+        </Dropdown>
+        {/* <BellOutlined
          style={{ fontSize: '24px',
          color: isCHanging ? '#F8F8F8' : '#D3D3D3',
          width: '15px'
           }}
           onMouseEnter={() => setIsChanging(true)}
           onMouseLeave={() => setIsChanging(false)}
-          />
+          /> */}
          </NavRight>  
          <NavRight>
           <img src="" alt="/" style={{width: "140px", height: "100px", borderRadius:"50%", border:"2px solid white"}} />
